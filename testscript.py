9b95fe32f1e5b2df4f04623b8973a48a29da0ce7
OptiTheNext <mail@hannesfreitag.de>
2022-05-01 19:29:03 +0000

    still trying to debug

---
+++ testscript.py
@@ -0,0 +1,18 @@
+import random
+import mysql.connector
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+mydb = mysql.connector.connect(host=os.environ.get('KRK_DB_HOST'),
+                                           user=os.environ.get('KRK_DB_USER'),
+                                           password=os.environ.get('KRK_DB_PASS'),
+                                           database=os.environ.get('KRK_DB_DATABASE'))
+
+cursor = mydb.cursor()
+cursor.execute("REPLACE INTO mcrc_tabelle (pat_id,dob,sex,diagnosis1,primary_location,status_fu,Kuerzel) VALUES ('12','2022-04-25','f','12','Zäkum','0','HFreitag')")
+mydb.commit()
+rows = cursor.fetchall()
+print(rows)
+print(cursor.rowcount)
