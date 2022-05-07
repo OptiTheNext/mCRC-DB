ab6a2788dc8395a8d53a4679f6d172caf8dfce39
Stefan Glorch <sglorch02@gmail.com>
2022-05-07 21:41:02 +0200

    Modified importDataIntoInputform
    Removed debugging logs
    Added checks to show spans with input

--- static/importDataIntoInputform.js
+++ static/importDataIntoInputform.js
@@ -3,15 +3,24 @@ function importData() {
     $.getJSON("/api/getDataForID", "pat_id_import="+pid, function(result){
         console.log(result);
         $.each(result, function(key, value) {
+            // Check if specific subfields of default-hidden spans have input
+            if(key === "diagnosis2") {
+                $("#diagnose2_check").prop('checked', Boolean(value)).trigger("change");
+            }
+            if(key === "pve_date") {
+                $("#pve_check").prop('checked', Boolean(value)).trigger("change");
+            }
+            if(key === "op_date_Surgery2") {
+                $("#secondOP_check").prop('checked', Boolean(value)).trigger("change");
+            }
+            if(key === "op_date_Surgery3") {
+                $("#thirdOP_check").prop('checked', Boolean(value)).trigger("change");
+            }
+
             if($('#'+key).prop("type") == "checkbox") {
-                console.log("Checkbox!");
-                console.log(key);
-                console.log(value);
                 $('#'+key).prop('checked', Boolean(value)).trigger("change");
             } else {
                 $('#'+key).val(value);
-                console.log(key);
-                console.log(value);
             }
         })
     })
