888c2a97d0e65a106c45763f06b05ff70677cf65
OptiTheNext <mail@hannesfreitag.de>
2022-05-19 18:49:45 +0000

    Added Diagnosis Tab in OutPut Fully functional

--- datenausgabe.py
+++ datenausgabe.py
@@ -72,10 +72,56 @@ def Analyse(parameters) -> pandas.DataFrame:
     if(parameters.get("diagnose_code_check",None)):
         if(parameters['diagnosis1']):
             print(type(parameters['diagnosis1']))
-            df.query("diagnosis1 == @parameters['diagnosis1']")
+            df.query("diagnosis1 == @parameters['diagnosis1']",inplace=True)
             print("checked for diagnosis1")
         if(parameters["diagnosis2"]):
-            df.query("diagnosis2 == @parameters['diagnosis2']")
+            df.query("diagnosis2 == @parameters['diagnosis2']",inplace=True)
             print("checked for diagnosis2")
-    print("hi")
+
+    ##CHeck for Diagnosis Date
+    if(parameters.get("diagnosis_date_check",None)):
+        if(parameters['von_diagnosis_date']):
+            von_diagnosis_date = datetime.datetime.strptime(parameters['von_diagnosis_date'], '%Y-%m-%d')
+            von_diagnosis_date = datetime.date(von_diagnosis_date.year,von_diagnosis_date.month,von_diagnosis_date.day)
+            df.query("diagnosis_date >= @von_diagnosis_date",inplace=True)
+        if(parameters["bis_diagnosis_date"]):
+            bis_diagnosis_date = datetime.datetime.strptime(parameters['bis_diagnosis_date'], '%Y-%m-%d')
+            bis_diagnosis_date = datetime.date(bis_diagnosis_date.year,bis_diagnosis_date.month,bis_diagnosis_date.day)
+            df.query("diagnosis_date <= @bis_diagnosis_date",inplace=True)
+    
+    ##Check for Primary Location
+    if(parameters.get('primary_location_check',None)):
+        locations = []
+        if(parameters.get('Rektum_check', None)): locations.append('Rektum')
+        if(parameters.get('Sigma_check', None)): locations.append('Sigma')
+        if(parameters.get('Descendens_check', None)): locations.append('Descendens')
+        if(parameters.get('Transversum_check', None)): locations.append('Transversum')
+        if(parameters.get('Ascendens_check', None)): locations.append('Ascendens')
+        if(parameters.get('Zäkum_check', None)): locations.append('Zäkum')
+        if(parameters.get('Rektosigmoid_check', None)): locations.append('Rektosigmoid')       
+        df.query("primary_location in @locations", inplace=True)
+
+    ##Check for Synchron/Metachron
+    if(parameters.get('crlm_met_syn_check',None)):
+        crlm_list = []
+        if(parameters.get('Synchron_check', None)): crlm_list.append('synchron');print("synchron")
+        if(parameters.get('Metachron_check', None)): crlm_list.append('metachron');print("metachron")
+        df.query("crlm_met_syn in @crlm_list", inplace=True)
+
+    ##check for Bilobulär#
+    if(parameters.get('Bilobulär_check',None)):
+        df.query("crlm_bilobular == 1", inplace=True) 
+        print("inside bilobulär")
+    
+    ##Check for Multimodal
+    if(parameters.get('Multimodal_check',None)):
+        df.query("multimodal == 1", inplace=True) 
+    
+    ##Check for twostaged
+    if(parameters.get('Two_staged_check',None)):
+        df.query("two_staged == 1", inplace=True) 
+       
+    
+
+    df.fillna("",inplace=True)
     return df
