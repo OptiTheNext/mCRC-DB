aadd65ae6d0ecd12de026691604e3086a807b1ad
OptiTheNext <mail@hannesfreitag.de>
2022-07-18 19:04:44 +0000

    Added new files, removed flask_session

---
+++ generate_token.py
@@ -0,0 +1,23 @@
+import os
+import jwt
+import datetime
+
+
+def generate_password_reset_token(username,password):
+    key = os.environ.get("KRK_APP_SECRET_KEY")
+    print(key)
+    delta = datetime.timedelta(hours=1)
+    now = datetime.datetime.utcnow()
+    expires = now + delta
+    exp = expires.timestamp()
+    encoded_jwt = jwt.encode(
+        {
+            "exp": exp,
+            "nbf": now,
+            "username": username,
+            "pwd":password
+        },
+        key,
+        algorithm="HS256",
+    )
+    return encoded_jwt 
