38ac7afb532eb16cf2df837aeb31b02896b256bd
OptiTheNext <mail@hannesfreitag.de>
2022-10-16 20:36:38 +0000

    Added small function to export to sql instead csv

--- backup_table.py
+++ backup_table.py
@@ -1,6 +1,10 @@
 import mysql.connector
 import datetime
 import os
+
+from os import listdir
+from os.path import isfile, join
+
 from dotenv import load_dotenv
 #Debugging
 import traceback
@@ -15,11 +19,35 @@ mydb = mysql.connector.connect(host=os.environ.get('KRK_DB_HOST'),
                                            password=os.environ.get('KRK_DB_PASS'),
                                            database=os.environ.get('KRK_DB_DATABASE'))
 
+#Es macht sinn, die letzte update datei zu importieren, und wenn diese sich von dem aktuellem stand erst dann ein backup zu machen, ansonsten müllt die Festplatte so voll
+
+onlyfiles = [f for f in listdir("./backups") if isfile(join("./backups", f))]
+print(onlyfiles)
+dates_to_check = []
+for x in onlyfiles:
+    date = x.split("_")[1]
+    date = date.split(".")[0]
+    date = datetime.datetime.strptime(date, "%b-%d-%Y")
+    print (date)
+    dates_to_check.append(date)
+
+youngest = max(dt for dt in dates_to_check if dt < datetime.datetime.now())
+print(youngest)
+
+path_to_newest_file = "./backups/mcrc.table.backup_" + youngest.strftime("%b-%d-%Y") +".csv"
+print(path_to_newest_file)
+
 cursor = mydb.cursor()
 df = cursor.execute("SELECT * FROM mcrc_tabelle")
 myresult = cursor.fetchall()
 df = pandas.DataFrame(myresult)
 df.columns = Columns.d 
-df.to_csv("./backups/mcrc_table.csv",date_format="%d.%m.%Y", sep=";")
+path = "./backups/mcrc.table.backup_" + datetime.date.today().strftime("%b-%d-%Y") +".csv"
+df.to_csv(path,date_format="%d.%m.%Y", sep=";")
+
+user = os.environ.get('KRK_DB_USER')
+password = os.environ.get('KRK_DB_PASS')
+
+os.system('mysqldump -u%s -p%s database > database.sql;' %(user,password))
         
 mydb.disconnect()
