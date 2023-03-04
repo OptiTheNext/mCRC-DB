7245e90ccc558c3e23a98af50a07d97302b24dba
Stefan Glorch <sglorch02@gmail.com>
2023-03-04 23:12:58 +0100

    Code cleanup

--- Scripts/testscript.py
+++ Scripts/testscript.py
@@ -1,14 +1,14 @@
-import random
-import mysql.connector
 import os
+
+import mysql.connector
 from dotenv import load_dotenv
 
 load_dotenv()
 
 mydb = mysql.connector.connect(host=os.environ.get('KRK_DB_HOST'),
-                                           user=os.environ.get('KRK_DB_USER'),
-                                           password=os.environ.get('KRK_DB_PASS'),
-                                           database=os.environ.get('KRK_DB_DATABASE'))
+                               user=os.environ.get('KRK_DB_USER'),
+                               password=os.environ.get('KRK_DB_PASS'),
+                               database=os.environ.get('KRK_DB_DATABASE'))
 
 cursor = mydb.cursor()
 cursor.execute("SELECT * FROM mcrc_tablle WHERE limax_second_date = 1.1.2000")
